pockata/mmutils

commit 20c5478c3935c5b350bf0cee31f983ca5ef46aa9

feat: Add d flag to return screen number
Use `d` to return the screen number that xrandr outputs when invoked
with --listmonitors.

Upon inspection of xrandr's code
(https://cgit.freedesktop.org/xorg/app/xrandr/tree/xrandr.c#n4041) the
screen number is taken from the order in which X returns screens.

mattr.c
@@ -11,7 +11,7 @@ static xcb_connection_t *conn;
 
 static void
 usage(char *name) {
-    fprintf(stderr, "usage: %s [-h] [-c] [nwhxy] <wid|mid>\n", name);
+    fprintf(stderr, "usage: %s [-h] [-c] [nwhxyd] <wid|mid>\n", name);
     exit(1);
 }
 
@@ -69,6 +69,9 @@ main (int argc, char *argv[]) {
                     break;
                 case 'y':
                     printf("%d", monitor.y);
+                    break;
+                case 'd':
+                    printf("%d", monitor.num);
                     break;
 
                 // Return the ID of the window/monitor that's passed as the

util.c
@@ -116,6 +116,7 @@ get_randr_monitors (xcb_connection_t *conn, monitor_t **monitors) {
                 is_primary = (int) primary_monitor->output == outputs[i];
             }
 
+            // TODO: Avoid the boolean trap
             mons[i] = (monitor_t) {
                 name,
                 ci_reply->x,
@@ -129,6 +130,7 @@ get_randr_monitors (xcb_connection_t *conn, monitor_t **monitors) {
         }
         else {
             // Otherwise mark it as 'not active'
+            // TODO: Avoid the boolean trap
             mons[i] = (monitor_t) { name, 0, 0, 0, 0, 0, 1, 0 };
         }
 
@@ -214,11 +216,19 @@ get_monitor_by_window_id(xcb_connection_t *conn, xcb_window_t pfw) {
 
     w = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, pfw), NULL);
 
+    // TODO: Avoid the boolean trap
     current_monitor = (monitor_t) { NULL, 0, 0, 0, 0, 0, 0, 0 };
     current_intersect = 0;
 
+    int inactive_monitors = 0;
+
     for (int i=0; i<num_monitors; i++) {
         m = monitors[i];
+
+        if (!m.active) {
+            inactive_monitors++;
+            continue;
+        }
 
         top = max(w->y, m.y);
         left = max(w->x, m.x);
@@ -228,6 +238,9 @@ get_monitor_by_window_id(xcb_connection_t *conn, xcb_window_t pfw) {
         if (bottom >= top && right >= left) {
             intersect = (bottom - top) * (right - left);
             if (intersect >= current_intersect) {
+                // set the monitor num to match xrandr screen number. the count
+                // excludes inactive monitors
+                m.num = i - inactive_monitors;
                 current_intersect = intersect;
                 current_monitor = m;
             }
@@ -245,15 +258,25 @@ get_monitor_by_name(xcb_connection_t *conn, char *name) {
 
     monitor_t m;
 
+    int inactive_monitors = 0;
     for (int i=0; i<num_monitors; i++) {
         m = monitors[i];
 
+        if (!m.active) {
+            inactive_monitors++;
+            continue;
+        }
+
         if (strcmp(m.name, name) == 0) {
+            // set the monitor num to match xrandr screen number. the count
+            // excludes inactive monitors
+            m.num = i - inactive_monitors;
             return m;
         }
     }
 
-    return (monitor_t) { NULL, 0, 0, 0, 0, 0, 0, 0 };
+    // TODO: Avoid the boolean trap
+    return (monitor_t) { NULL, 0, 0, 0, 0, 0, 0, 0, 0 };
 }
 
 xcb_window_t

util.h
@@ -1,6 +1,7 @@
 typedef struct monitor_t {
     char *name;
     int x, y, width, height, active, connected, primary;
+    int num;
 } monitor_t;
 
 xcb_visualid_t get_visual (void);

commit bd7091ae8a1d7f3d0c4283fde83afd30ab1de874

fix: Fix `mattr -c`
Fixes a regression introduced in
20c5478c3935c5b350bf0cee31f983ca5ef46aa9

mattr.c
@@ -56,6 +56,10 @@ main (int argc, char *argv[]) {
     for (cnt=2; argv[cnt]; cnt++) {
         monitor = get_monitor(conn, argv[cnt]);
 
+        if (!monitor.active) {
+            errx(1, "monitor %s not active", argv[cnt]);
+        }
+
         // no monitor found
         if (monitor.name == NULL) {
             errx(1, "%s: no such monitor", argv[cnt]);

util.c
@@ -238,7 +238,6 @@ get_monitor_by_window_id(xcb_connection_t *conn, xcb_window_t pfw) {
 
         if (!m.active) {
             inactive_monitors++;
-            continue;
         }
 
         top = max(w->y, m.y);
@@ -275,7 +274,6 @@ get_monitor_by_name(xcb_connection_t *conn, char *name) {
 
         if (!m.active) {
             inactive_monitors++;
-            continue;
         }
 
         if (strcmp(m.name, name) == 0) {